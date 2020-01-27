d66abedd58e445fc21f2a8601a01e855d74f354b
Brandon Stephens <brandonstephens@me.com>
2020-01-27 15:17:26 -0500

    create bootstrap in try catch

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -24,8 +24,12 @@ exports.getBootcamp = (req, res, next) => {
 // @route      POST /api/v1/bootcamps
 // @access     Private
 exports.createBootcamp = async (req, res, next) => {
-  const bootcamp = await Bootcamp.create(req.body)
-  res.status(201).json({ success: true, data: bootcamp })
+  try {
+    const bootcamp = await Bootcamp.create(req.body)
+    res.status(201).json({ success: true, data: bootcamp })
+  } catch (err) {
+    res.status(400).json({ success: false })
+  }
 }
 
 // @desc       Update a bootcamp
