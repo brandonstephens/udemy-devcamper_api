e103e637188a9aeb922f0917e1b05a767df22ee9
Brandon Stephens <brandonstephens@me.com>
2020-01-27 10:26:49 -0500

    stub in bootcamp crud routes

--- server.js
+++ server.js
@@ -8,19 +8,46 @@ dotenv.config({
 
 const app = express()
 
-app.get('/', (req, res) => {
-  // res.send({ msg: '🤪 Yellow from express!!!'})
-  // res.sendStatus(400)
-  // res.status(400).json({
-  //   success: false,
-  //   errors: '😕'
-  // })
-
+// get all bootcamps
+app.get('/api/v1/bootcamps', (req, res) => {
   res.status(200).json({
     success: true,
-    id: 'yey'
+    msg: '@todo return all bootcamps'
   })
 })
+
+// update existing bootcamp
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `@todo return ${req.params.id} bootcamp`
+  })
+})
+
+// add new bootcamp
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: '@todo add new bootcamp'
+  })
+})
+
+// update existing bootcamp
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `@todo update ${req.params.id} bootcamp`
+  })
+})
+
+// delete bootcamp
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `@todo delete ${req.params.id} bootcamp`
+  })
+})
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`😀 server running in ${process.env.NODE_ENV} on ${process.env.PORT}`))
