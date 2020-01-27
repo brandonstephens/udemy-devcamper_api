76ab7a89d9fb0dc4c63fe1fa5f1916fe954523f9
Brandon Stephens <brandonstephens@me.com>
2020-01-27 15:58:23 -0500

    add get single bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,21 +3,31 @@ const Bootcamp = require('../models/Bootcamp')
 // @desc       Get all bootcamps
 // @route      GET /api/v1/bootcamps
 // @access     Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: '@todo return all bootcamps',
-  })
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find()
+    res.status(200).json({ success: true, data: bootcamps })
+  } catch (err) {
+    res.status(400).json({ success: false })
+  }
 }
 
 // @desc       Get single bootcamp
 // @route      GET /api/v1/bootcamps/:id
 // @access     Public
-exports.getBootcamp = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: `@todo return ${req.params.id} bootcamp`,
-  })
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id)
+
+    // if valid id doesn't exist
+    if (!bootcamp) {
+      return res.status(400).json({ success: false })
+    }
+
+    res.status(200).json({ success: true, data: bootcamp })
+  } catch (err) {
+    res.status(400).json({ success: false })
+  }
 }
 
 // @desc       Create a bootcamp
