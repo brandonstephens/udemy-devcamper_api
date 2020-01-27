cdf5bc3b63086af142e346d437251e11b8dd00e2
Brandon Stephens <brandonstephens@me.com>
2020-01-27 10:19:34 -0500

    playing about with response status

--- server.js
+++ server.js
@@ -6,7 +6,21 @@ dotenv.config({
   path: './config/config.env'
 })
 
-// Create and run server
 const app = express()
+
+app.get('/', (req, res) => {
+  // res.send({ msg: '🤪 Yellow from express!!!'})
+  // res.sendStatus(400)
+  // res.status(400).json({
+  //   success: false,
+  //   errors: '😕'
+  // })
+
+  res.status(200).json({
+    success: true,
+    id: 'yey'
+  })
+})
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`😀 server running in ${process.env.NODE_ENV} on ${process.env.PORT}`))
