d6dd1f5a57df2f180f3fb3db4d103bdd97b041db
Brandon Stephens <brandonstephens@me.com>
2020-02-16 14:49:02 -0500

    cascade delete and virtuals ch 39

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -20,7 +20,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   queryString = queryString.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
 
   // find resources
-  query = Bootcamp.find(JSON.parse(queryString))
+  query = Bootcamp.find(JSON.parse(queryString)).populate('courses')
 
   // select (filter)
   if (req.query.select) {
@@ -110,11 +110,13 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @route      DELETE /api/v1/bootcamps/:id
 // @access     Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
+  const bootcamp = await Bootcamp.findById(req.params.id)
 
   if (!bootcamp) {
     return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
   }
+
+  bootcamp.remove()
 
   res.status(200).json({ success: true, data: {} })
 })
--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -2,93 +2,99 @@ const mongoose = require('mongoose')
 const slugify = require('slugify')
 const geocoder = require('../utils/geocoder')
 
-const BootcampSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Name is required'],
-    unique: true,
-    trim: true,
-    maxlength: [50, 'Name cannot be more than 50 characters'],
-  },
-  slug: String,
-  description: {
-    type: String,
-    required: [true, 'Description is required'],
-    maxlength: [500, 'Name cannot be more than 500 characters'],
-  },
-  website: {
-    type: String,
-    match: [
-      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
-      'Must be valid url with protocol (http or https)',
-    ],
-  },
-  phone: {
-    type: String,
-    maxlength: [20, 'Phone number cannot be longer than 20 characters'],
-  },
-  email: {
-    type: String,
-    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Email must be valid'],
-  },
-  address: {
-    type: String,
-    required: [true, 'Address required'],
-  },
-  location: {
-    // GeoJSON Point
-    type: {
+const BootcampSchema = new mongoose.Schema(
+  {
+    name: {
       type: String,
-      enum: ['Point'],
+      required: [true, 'Name is required'],
+      unique: true,
+      trim: true,
+      maxlength: [50, 'Name cannot be more than 50 characters'],
     },
-    coordinates: {
-      type: [Number],
-      index: '2dsphere',
+    slug: String,
+    description: {
+      type: String,
+      required: [true, 'Description is required'],
+      maxlength: [500, 'Name cannot be more than 500 characters'],
     },
-    formattedAddress: String,
-    street: String,
-    city: String,
-    state: String,
-    zipcode: String,
-    country: String,
+    website: {
+      type: String,
+      match: [
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+        'Must be valid url with protocol (http or https)',
+      ],
+    },
+    phone: {
+      type: String,
+      maxlength: [20, 'Phone number cannot be longer than 20 characters'],
+    },
+    email: {
+      type: String,
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Email must be valid'],
+    },
+    address: {
+      type: String,
+      required: [true, 'Address required'],
+    },
+    location: {
+      // GeoJSON Point
+      type: {
+        type: String,
+        enum: ['Point'],
+      },
+      coordinates: {
+        type: [Number],
+        index: '2dsphere',
+      },
+      formattedAddress: String,
+      street: String,
+      city: String,
+      state: String,
+      zipcode: String,
+      country: String,
+    },
+    careers: {
+      // Array of strings
+      type: [String],
+      required: true,
+      enum: ['Web Development', 'Mobile Development', 'UI/UX', 'Data Science', 'Business', 'Other'],
+    },
+    averageRating: {
+      type: Number,
+      min: [1, 'Rating must be at least 1'],
+      max: [10, 'Rating must cannot be more than 10'],
+    },
+    averageCost: Number,
+    photo: {
+      type: String,
+      default: 'no-photo.jpg',
+    },
+    housing: {
+      type: Boolean,
+      default: false,
+    },
+    jobAssistance: {
+      type: Boolean,
+      default: false,
+    },
+    jobGuarantee: {
+      type: Boolean,
+      default: false,
+    },
+    acceptGi: {
+      type: Boolean,
+      default: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  careers: {
-    // Array of strings
-    type: [String],
-    required: true,
-    enum: ['Web Development', 'Mobile Development', 'UI/UX', 'Data Science', 'Business', 'Other'],
-  },
-  averageRating: {
-    type: Number,
-    min: [1, 'Rating must be at least 1'],
-    max: [10, 'Rating must cannot be more than 10'],
-  },
-  averageCost: Number,
-  photo: {
-    type: String,
-    default: 'no-photo.jpg',
-  },
-  housing: {
-    type: Boolean,
-    default: false,
-  },
-  jobAssistance: {
-    type: Boolean,
-    default: false,
-  },
-  jobGuarantee: {
-    type: Boolean,
-    default: false,
-  },
-  acceptGi: {
-    type: Boolean,
-    default: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-})
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+)
 
 // create bootcamp slug from the name
 BootcampSchema.pre('save', function(next) {
@@ -113,6 +119,23 @@ BootcampSchema.pre('save', async function(next) {
   // Do not save address in DB
   this.address = undefined
   next()
+})
+
+// Cascade delete courses when a bootcamp is deleted
+BootcampSchema.pre('remove', async function(next) {
+  console.log(`Courses being removed from bootcamp ${this._id}`)
+  await this.model('Course').deleteMany({
+    bootcamp: this._id,
+  })
+  next()
+})
+
+// Reverse populate with virtuals
+BootcampSchema.virtual('courses', {
+  ref: 'Course',
+  localField: '_id',
+  foreignField: 'bootcamp',
+  justOne: false,
 })
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema)
