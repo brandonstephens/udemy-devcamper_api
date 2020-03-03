dd02f62dd62ba5df928c146a3617cc9804e33411
Brandon Stephens <brandonstephens@me.com>
2020-03-03 12:31:29 -0500

    sending email to mailtrap

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
+const sendEmail = require('../utils/sendEmail')
 const User = require('../models/User')
 
 // @desc       Register user
@@ -70,6 +71,29 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
   const resetToken = user.getResetPasswordToken()
 
   await user.save({ validateBeforeSave: false })
+
+  // create reset url
+  const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`
+  const message = `A password reset for your email has been requested. \n\n ${resetUrl}`
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'DevCamper Password Reset',
+      message,
+    })
+
+    res.status(200).json({ success: true, data: 'Email sent' })
+  } catch (err) {
+    console.log(err)
+
+    user.resetPasswordToken = undefined
+    user.resetPasswordExpire = undefined
+
+    await user.save({ validateBeforeSave: false })
+
+    return next(new ErrorResponse('Email could not be sent', 500))
+  }
 
   res.status(200).json({ success: true, data: user })
 })
