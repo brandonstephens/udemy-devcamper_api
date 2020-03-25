eac055a94c572c50a79e76952bbba0192ddf93ff
Brandon Stephens <brandonstephens@me.com>
2020-03-25 13:36:13 -0400

    add and avg reviews

--- models/Review.js
+++ models/Review.js
@@ -33,4 +33,40 @@ const ReviewSchema = new mongoose.Schema({
   },
 })
 
+// Limit user to 1 review to per bootcamp
+ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true })
+
+// average rating
+ReviewSchema.statics.getAverageRating = async function(bootcampId) {
+  const data = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageRating: { $avg: '$rating' },
+      },
+    },
+  ])
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageRating: data[0].averageRating,
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+// Update average cost after save
+ReviewSchema.post('save', function() {
+  this.constructor.getAverageRating(this.bootcamp)
+})
+
+// Update average cost after remove
+ReviewSchema.post('remove', function() {
+  this.constructor.getAverageRating(this.bootcamp)
+})
+
 module.exports = mongoose.model('Review', ReviewSchema)
